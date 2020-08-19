a02d39bf5645da43897e14fac56f2f3a78bfa22d
Francisco Rogido <franrogi30@gmail.com>
2020-08-18 23:27:15 -0300

    pagina de error

--- site/soundPower/app.js
+++ site/soundPower/app.js
@@ -32,8 +32,8 @@ app.use('/contact', contactRouter)
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use ((req, res, next) => {
+  res.status(404).render("not-found")
 });
 
 // error handler
