0a43df88fa048345326abdfaba373c0580f01eb8
unknown <rociomilagroslima@outlook.es>
2020-10-15 11:15:24 -0300

    validaciones sin terminar

--- site/soundPower/controllers/usersController.js
+++ site/soundPower/controllers/usersController.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const dbusers = require('../data/dbusers');
-
+const {check, validationResult,body} = require('express-validator')
 module.exports = {
     'register' : (req, res) => {
 
-        let id = dbusers.length;
 
+        let id = dbusers.length;
         //Crear nuevos usuarios        
         let newUser ={
-            id: id + 1,
+            id: id,
             email: req.body.email,
             password:bcrypt.hashSync(req.body.pass, 10),
             confirmpassword:bcrypt.hashSync(req.body.cpass,10),
@@ -27,6 +27,7 @@ module.exports = {
         res.render('register',{
             title:"Registro de usuarios"
         })
+   
     },
     /*'save' :(req,res) =>{
         res.render('register')
--- site/soundPower/routes/users.js
+++ site/soundPower/routes/users.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/usersController.js');
-
+const {check, validationResult, body} = require('express-validator')
 /* POST register page */
 router.get('/register', controller.register);
-router.post('/register',controller.register)
+//Validaciones
+router.post('/register',[
+    check('email').isEmail(),
+    check('pass').isLength({min:8}),
+    check('cpass').isLength({min:8}),
+    check('fname').isLength(),
+    check('lname').isLength(),
+    check('phone').isInt({min:0}),
+    check('address').isLength()
+],controller.register);
 //router.post('/register',controller.save)
 router.get('/login', controller.Login)
 
